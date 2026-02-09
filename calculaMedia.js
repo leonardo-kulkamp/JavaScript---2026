6da92e696ac57ebd8458d9a078cd2cbaa807e612
Leonardo Kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2026-02-09 13:05:24 +0000

    js

---
+++ calculaMedia.js
@@ -0,0 +1,11 @@
+let nota1 = 8;
+let nota2 = 8;
+let nota3 = 9;
+let nota4 = 10;
+
+const nomeAluno = "Olliver";
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log ("O aluno: " + nomeAluno);
+console.log ("Tem a média: " + media);
